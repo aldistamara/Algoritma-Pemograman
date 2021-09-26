aafa7ee7c6955656fe4e800efcdb1ab9c4d509b4
aldistamara <91387425+aldistamara@users.noreply.github.com>
2021-09-26 17:41:08 +0700

    Modul 1 - Latihan Kedua

---
+++ latihankedua.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+@Materi: Variabel dan Pemograman Dasar
+@Judul: Praktikum 1 Materi
+@Hari/Tanggal: Senin, 20210920
+@NIM: 065002100021
+@author: Aldis Tamara Putri Iskandar
+
+"""
+print('Menghitung Luas Musholla dengan Satuan Meter')
+
+p = float(input('Berapa panjang musholla ini? '))
+l = float(input('Berapa lebar musholla ini? '))
+L = p*l
+print('Jadi, luas musholla ini adalah ', str(L), 'm2')
